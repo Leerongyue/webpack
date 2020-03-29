449a0a4c212b0a0ed245c8f53d1600f8941254c9
lirongyue <761780862@qq.com>
2020-03-29 22:46:40 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-  
-npm run build &&
+npm run build && 
 git checkout gh-pages &&
 rm -rf *.html *.js *.css *.png &&
 mv dist/* ./ &&
-rm -rf dist;
-git add -A . &&
+rm -rf dist
+git add . &&
 git commit -m 'update' &&
-git push
+git push &&
+git checkout -
